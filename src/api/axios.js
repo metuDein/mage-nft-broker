97e662d9ccfebea1020dd71ab292a3bfc1e4eac9
metuDein <119444184+metuDein@users.noreply.github.com>
2023-07-03 00:15:03 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3500';
-// const BASE_URL = 'https://app-tritonsprime.onrender.com';
+//const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://app-tritonsprime.onrender.com';
 
 export default axios.create({
     baseURL : BASE_URL,
