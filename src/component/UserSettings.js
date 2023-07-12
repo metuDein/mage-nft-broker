0bf809b83bb208a31f4c3e009fe8ed2e09ab08fb
metudein <maxmetadein@gmail.com>
2023-07-12 16:58:29 +0100

    update

--- src/component/UserSettings.js
+++ src/component/UserSettings.js
@@ -79,6 +79,7 @@ const UserSettings = () => {
         e.preventDefault()
 
         const ethereum = window.ethereum;
+        
         if (!ethereum) return window.alert('no metamask wallet found please switch to metamask wallet mobile app\'s browser');
 
         const connect = await ethereum.request({ method: 'eth_requestAccounts' });
